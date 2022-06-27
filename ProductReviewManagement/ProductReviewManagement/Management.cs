092220ff0e6222da940e54f9cf0dec245bc5e51c
chinki kumari singh <singhchinkikumari.com>
2022-06-27 17:17:56 +0530

    [Chinki] retrive top three record

---
+++ ProductReviewManagement/ProductReviewManagement/Management.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+         public void display(List<ProductReview> recordData)
+         {
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product id = " + list.ProductId + "User id = " + list.UserId + "Rating is = " + list.Rating + " Review is = " + list.Review + " isLike = " + list.isLike);
+            }
+         }
+        public void topRecords(List<ProductReview> listProductReviews)
+        {
+            var recordData = (from productReview in listProductReviews orderby productReview.Rating descending select productReview).Take(3).ToList();
+            Console.WriteLine("\n Top 3 records = ");
+            display(recordData);
+        }
+    }
+}
